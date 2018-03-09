f1fd722e6ffd8c418b3a86e5837c87a061383f85
Splinter Suidman <splinter@mannenopdemaan.nl>
2018-03-09 17:33:35 +0100

    Remove unused imports in C code.

--- plugin/plugin.cpp
+++ plugin/plugin.cpp
@@ -1,20 +1,13 @@
-#include <stdlib.h>
-#include <string.h>
-
 #include "../chunkwm/src/api/plugin_api.h"
-#include "../chunkwm/src/common/accessibility/application.h"
-#include "../chunkwm/src/common/accessibility/window.h"
 
 extern "C" const char *chunkwm_rust_get_name(void);
 extern "C" const char *chunkwm_rust_get_version(void);
 extern "C" plugin *get_plugin();
 
-#define internal static
+static const char *PluginName = chunkwm_rust_get_name();
+static const char *PluginVersion = chunkwm_rust_get_version();
 
-internal const char *PluginName = chunkwm_rust_get_name();
-internal const char *PluginVersion = chunkwm_rust_get_version();
-
-CHUNKWM_EXTERN
+extern "C"
 {
     plugin *GetPlugin()
     {
